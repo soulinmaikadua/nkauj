e4d288aefe3f1bc6abf9f20501eeb0821bca4c11
soulin maikadua <104555691+soulincsl@users.noreply.github.com>
2024-03-20 17:27:55 +0700

    office commit

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,33 @@
 
 export const SITE_TITLE = "Nkauj hmoob";
 export const SITE_DESCRIPTION = "Nkauj hmoob zoo siab txhais tos!";
+export const ARTISTS = [
+  {
+    name: "Kawm muas",
+    value: "kawm-muas",
+  },
+  {
+    name: "Laib laus",
+    value: "laib-laus",
+  },
+  {
+    name: "Tsom xyooj",
+    value: "tsom-xyooj",
+  },
+  {
+    name: "Xab Thoj",
+    value: "xab-thoj",
+  },
+  {
+    name: "Kub Qav Kaws Vaj",
+    value: "kub-qav-kaws-vaj",
+  },
+  {
+    name: "Pov Thoj",
+    value: "pov-thoj",
+  },
+  {
+    name: "MR. FBI Yim Lee",
+    value: "fbi-yim-lee",
+  },
+];
