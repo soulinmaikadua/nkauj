d91fefde61b23d1e839fc8d887a0ca20c93f5335
soulin maikadua <104555691+soulincsl@users.noreply.github.com>
2024-03-20 09:51:43 +0700

    office commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	// site: 'nkauj.com',
+	site: 'nkaujhmoob.netlify.app',
 	output: "server",
 	adapter: netlify(),
 	// base: "nkauj",
