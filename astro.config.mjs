9ed064c19ba82be95213c2c1fa45d1e91adba15b
soulin maikadua <104555691+soulincsl@users.noreply.github.com>
2024-03-20 09:04:22 +0700

    office commig

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://soulinmaikadua.github.io',
-	base: "nkauj",
+	site: 'nkauj.com',
+	// base: "nkauj",
 	integrations: [mdx(), sitemap(), tailwind()]
 });
