eec26e82a76ce9fe375000b6ed43e09859954659
soulin maikadua <104555691+soulincsl@users.noreply.github.com>
2024-03-20 14:55:34 +0700

    office commit laib laus

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Nkauj";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Nkauj hmoob";
+export const SITE_DESCRIPTION = "Nkauj hmoob zoo siab txhais tos!";
