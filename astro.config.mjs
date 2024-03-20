5a05288a7f2035e10e2b76acdeb40988d5000e7f
soulin maikadua <104555691+soulincsl@users.noreply.github.com>
2024-03-20 09:37:01 +0700

    office commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import netlify from '@astrojs/netlify';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'nkauj.com',
+	// site: 'nkauj.com',
+	output: "server",
+	adapter: netlify(),
 	// base: "nkauj",
 	integrations: [mdx(), sitemap(), tailwind()]
 });
