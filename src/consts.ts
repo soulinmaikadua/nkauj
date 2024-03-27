a5fe668bee41354f65d59bd79d9d14e90d7cefca
soulin maikadua <104555691+soulincsl@users.noreply.github.com>
2024-03-27 09:33:24 +0700

    google added

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Nkauj hmoob";
+export const SITE_TITLE = "Nkauj";
 export const SITE_DESCRIPTION = "Nkauj hmoob zoo siab txhais tos!";
 export const ARTISTS = [
   {
