a79ecf84238b50ef64088ca41d29902894b3d086
soulin maikadua <104555691+soulincsl@users.noreply.github.com>
2025-01-08 10:08:20 +0700

    office update

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/laib-laus/${post.slug}/`,
+			link: `/laib-laus/${post.slug}`,
 		})),
 	});
 }
